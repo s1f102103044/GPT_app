39b254f6f7621fb08d046f94a8d81483d8e5309b
s1f102103044 <85671690+s1f102103044@users.noreply.github.com>
2023-11-29 14:35:33 +0900

    途中

--- video_site/urls.py
+++ video_site/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-#from django.contrib import admin
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.register, name='register'), 
 
     path('top',views.top,name='top'),
