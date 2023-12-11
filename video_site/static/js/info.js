270773bf87fcf1c14ce7fbf436cf714b48947b5f
s1F102103954 <85662020+s1F102103954@users.noreply.github.com>
2023-12-11 15:37:21 +0900

    お問い合わせページ基盤作成
    お問い合わせフォーム欄使用のためにjsとcssいじっちゃってるので好き勝手修正してください

---
+++ video_site/static/js/info.js
@@ -0,0 +1,27 @@
+// When the browser is ready...
+$(function() {
+    // validate
+    $("#contact").validate({
+        // Set the validation rules
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            message: "required",
+        },
+        // Specify the validation error messages
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter a valid email address",
+            message: "Please enter a message",
+        },
+        // submit handler
+        submitHandler: function(form) {
+          //form.submit();
+           $(".message").show();
+           $(".message").fadeOut(4500);
+        }
+    });
+  });
