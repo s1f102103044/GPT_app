bb201a16d913aa87446d4c66694c43db21fe6bf4
s1f4784 <85672423+s1f4784@users.noreply.github.com>
2024-01-10 15:53:26 +0900

    修正

--- video_site/static/js/top.js
+++ video_site/static/js/top.js
@@ -10,3 +10,50 @@ document.addEventListener("DOMContentLoaded", function () {
         menuContent.style.display = "none"; // メニューを非表示
     });
 });
+
+
+//スクロールすると上部に固定させるための設定を関数でまとめる
+function FixedAnime() {
+    var headerH = $('#header').outerHeight(true);
+    var scroll = $(window).scrollTop();
+    if (scroll >= headerH){//headerの高さ以上になったら
+        $('#header').addClass('fixed');//fixedというクラス名を付与
+      }else{//それ以外は
+        $('#header').removeClass('fixed');//fixedというクラス名を除去
+      }
+  }
+
+//ナビゲーションをクリックした際のスムーススクロール
+$('#g-navi a').click(function () {
+    var elmHash = $(this).attr('href'); //hrefの内容を取得
+    var pos = Math.round($(elmHash).offset().top-120);  //headerの高さを引く
+    $('body,html').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
+    return false;//リンクの無効化
+  });
+  
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+  });
+  
+  // ページが読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function () {
+    FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+  });
+
+
+// フェード
+const fade = document.querySelector('.fade');
+
+const fadeKeyframes = {
+  opacity: [0, 1],
+}
+
+const fadeOptions = {
+  duration: 3000,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+fade.animate(fadeKeyframes, fadeOptions);
